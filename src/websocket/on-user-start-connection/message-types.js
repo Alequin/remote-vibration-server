2fed869d683f70991069488709dc6d13889385c5
Alequin <j-cox03@hotmail.com>
2021-03-24 01:15:35 +0000

    Added a message type which is sent as a confirmation of success to those who send vibration patterns

--- src/websocket/on-user-start-connection/message-types.js
+++ src/websocket/on-user-start-connection/message-types.js
@@ -1,5 +1,7 @@
 module.exports = Object.freeze({
   connectToRoom: "connectToRoom",
   sendMessage: "sendMessage",
-  sendVibrationPattern: "sendVibrationPattern",
+  sendVibrationPattern: "sendVibrationPattern", // sent by client when send a vibration pattern
+  receivedVibrationPattern: "receivedVibrationPattern", // sent by server to recipients of a vibration pattern
+  confirmVibrationPatternSent: "confirmVibrationPatternSent", // sent by server in response to send to confirm message was sent
 });
