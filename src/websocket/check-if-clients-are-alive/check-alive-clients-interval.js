502ebbb316f4bdf7d5f0e11c8a75c129d438bcb4
Alequin <j-cox03@hotmail.com>
2021-04-25 20:56:50 +0100

    Increase ping-pong period

--- src/websocket/check-if-clients-are-alive/check-alive-clients-interval.js
+++ src/websocket/check-if-clients-are-alive/check-alive-clients-interval.js
@@ -1,5 +1,5 @@
 const toMilliseconds = require("../../to-milliseconds");
 
-const checkAliveClientsInterval = () => toMilliseconds.seconds(60);
+const checkAliveClientsInterval = () => toMilliseconds.seconds(1);
 
 module.exports = { checkAliveClientsInterval };
